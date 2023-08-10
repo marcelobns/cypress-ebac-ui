fdca34e14596ea6b923b2fd2dfe89db152631d21
marcelobns <marcelobns@live.com>
2023-08-09 20:08:59 -0400

    Usando Page Objects; Gerando reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'i5i9bt',
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br/",
     setupNodeEvents(on, config) {
--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" /> 
+import EnderecoPage from '../support/pages/endereco.page'
+import dsEnderecos from '../fixtures/enderecos.json'
 
 describe('Funcionalidade de Endereço - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -9,5 +11,15 @@ describe('Funcionalidade de Endereço - Faturamento e Entrega', () => {
         })
     })
 
-    it('Deve fazer cadastro de faturamento com sucesso', () => {})
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Maria', 'Silva', 'Empresa Teste', 'Brasil', 'Rua dos Bobos', 'zero', 'Boa Vista', 'Roraima', '69000000', '95 999999999', 'email@company.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    })
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        dsEnderecos.forEach(endereco => {
+            EnderecoPage.editarEnderecoFaturamento({...endereco})
+            cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+        });
+    })
 })
